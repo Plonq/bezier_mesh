a2de0935604f09dda81336d286154616942abeb6
Huon Imberger <huon@plonq.org>
2023-05-04 17:55:55 +1000

    Mesh triangles working

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,13 @@ use std::f32::consts::{PI, TAU};
 
 fn main() {
     App::new()
-        .insert_resource(Config { detail: 20 })
+        .insert_resource(Config {
+            detail: 20,
+            control_points: (0..4)
+                .map(|i| Vec3::new(i as f32 * 3.0, 0.0, 0.0))
+                .collect(),
+            ..default()
+        })
         .register_type::<Config>()
         .add_plugins(DefaultPlugins)
         .add_plugin(PanOrbitCameraPlugin)
@@ -26,13 +32,22 @@ fn main() {
         .add_plugin(WorldInspectorPlugin::default())
         .add_plugins(DefaultPickingPlugins)
         .add_plugin(TransformGizmoPlugin::default())
-        .add_startup_systems((setup, build_mesh).chain())
-        // .add_system(debug)
+        .add_startup_system(setup)
+        .add_systems(
+            (
+                clear.run_if(|config: Res<Config>| config.auto_update),
+                build_mesh.run_if(|config: Res<Config>| config.auto_update),
+            )
+                .chain(),
+        )
         .run()
 }
 
 #[derive(Component, Default, Debug)]
 struct ControlPoint(usize);
+
+#[derive(Component, Default, Debug)]
+struct Generated;
 
 #[derive(Component, Default, Debug, Reflect)]
 #[reflect(Component)]
@@ -41,7 +56,9 @@ struct Velocity(Vec2);
 #[derive(Reflect, Resource, Default, InspectorOptions)]
 #[reflect(Resource, InspectorOptions)]
 struct Config {
+    auto_update: bool,
     detail: usize,
+    control_points: Vec<Vec3>,
 }
 
 fn setup(
@@ -50,19 +67,6 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     config: Res<Config>,
 ) {
-    // Ground
-    // commands.spawn(PbrBundle {
-    //     mesh: meshes.add(shape::Plane::from_size(5.0).into()),
-    //     material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-    //     ..default()
-    // });
-    // Cube
-    // commands.spawn(PbrBundle {
-    //     mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-    //     material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-    //     transform: Transform::from_xyz(0.0, 0.5, 0.0),
-    //     ..default()
-    // });
     // Light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -87,12 +91,8 @@ fn setup(
         GizmoPickSource::default(),
     ));
 
-    let control_points = (0..4)
-        .map(|i| Vec3::new(i as f32 * 3.0, 0.0, 0.0))
-        .collect::<Vec<_>>();
-
     // Control point meshes
-    for (i, point) in control_points.iter().enumerate() {
+    for (i, point) in config.control_points.iter().enumerate() {
         commands.spawn((
             ControlPoint(i),
             PbrBundle {
@@ -108,95 +108,30 @@ fn setup(
             GizmoTransformable,
         ));
     }
+}
 
-    let p1 = control_points[0];
-    let p2 = control_points[1];
-    let p3 = control_points[2];
-    let p4 = control_points[3];
-    let vertices = (0..=config.detail)
-        .map(|i| i as f32 / config.detail as f32)
-        .map(|t| (t, cubic_bezier(p1, p2, p3, p4, t)))
-        .flat_map(|(t, curve_point)| {
-            // Vertices of one slice of road, relative to the point on the curve
-            let local_vertices = vec![
-                Vec3::new(-0.5, 0.3, 0.0),
-                Vec3::new(-0.3, 0.3, 0.0),
-                Vec3::new(-0.2, 0.2, 0.0),
-                Vec3::new(0.2, 0.2, 0.0),
-                Vec3::new(0.3, 0.3, 0.0),
-                Vec3::new(0.5, 0.3, 0.0),
-                Vec3::new(0.5, 0.0, 0.0),
-                Vec3::new(-0.5, 0.0, 0.0),
-            ];
-            // Map these local points to world points by adding them to the curve point
-            local_vertices.into_iter().map(move |local_vertex| {
-                let prev = cubic_bezier(p1, p2, p3, p4, t - 0.01);
-                let next = cubic_bezier(p1, p2, p3, p4, t + 0.01);
-                // Create little local space basis vectors, pointing along the curve
-                let local_z = (prev - next).normalize();
-                let local_y = Vec3::Y;
-                let local_x = local_y.cross(local_z).normalize();
-                // Convert local coordinates to world coordinates
-                let mut world_vertex = curve_point;
-                world_vertex += local_vertex.x * local_x;
-                world_vertex += local_vertex.y * local_y;
-                world_vertex += local_vertex.z * local_z;
-
-                world_vertex
-            })
-        })
-        .collect::<Vec<_>>();
-
-    // debug
-    for v in vertices.iter() {
-        println!("spawning at: {v}");
-        commands.spawn((PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::UVSphere {
-                radius: 0.02,
-                ..default()
-            })),
-            material: materials.add(Color::RED.into()),
-            transform: Transform::from_translation(*v),
-            ..default()
-        },));
+fn clear(mut commands: Commands, generated_q: Query<Entity, With<Generated>>) {
+    for gen in generated_q.iter() {
+        commands.entity(gen).despawn();
     }
-
-    let triangles: Vec<u32> = vec![0, 8, 15, 0, 15, 7, 0, 9, 8, 0, 1, 9];
-
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
-    mesh.set_indices(Some(Indices::U32(triangles)));
-    mesh.duplicate_vertices();
-    mesh.compute_flat_normals();
-    let handle = meshes.add(mesh);
-
-    commands.spawn(PbrBundle {
-        mesh: handle,
-        material: materials.add(StandardMaterial {
-            base_color: Color::ORANGE_RED,
-            ..default()
-        }),
-        ..default()
-    });
 }
 
 fn build_mesh(
-    config: Res<Config>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    point_q: Query<(&ControlPoint, &Transform), With<ControlPoint>>,
+    point_q: Query<(&ControlPoint, &Transform)>,
+    config: Res<Config>,
 ) {
-    if let Some((tfm1, tfm2, tfm3, tfm4)) = point_q
+    // println!("Building mesh...");
+    if let Some(((_, tfm1), (_, tfm2), (_, tfm3), (_, tfm4))) = point_q
         .iter()
-        .sorted_by_key(|(p, _)| p.0)
-        .map(|(_, tfm)| tfm)
+        .sorted_by_key(|(cp, _)| cp.0)
         .tuples::<(_, _, _, _)>()
         .last()
     {
-        println!("Has transforms");
-        let vertices = (0..=config.detail)
-            .map(|i| i as f32 / config.detail as f32)
+        let vertices = (0..config.detail)
+            .map(|i| i as f32 / (config.detail as f32 - 1.0))
             .map(|t| {
                 (
                     t,
@@ -254,19 +189,46 @@ fn build_mesh(
 
         // debug
         for v in vertices.iter() {
-            println!("spawning at: {v}");
-            commands.spawn((PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::UVSphere {
-                    radius: 0.02,
+            // println!("spawning at: {v}");
+            commands.spawn((
+                PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::UVSphere {
+                        radius: 0.02,
+                        ..default()
+                    })),
+                    material: materials.add(Color::RED.into()),
+                    transform: Transform::from_translation(*v),
                     ..default()
-                })),
-                material: materials.add(Color::RED.into()),
-                transform: Transform::from_translation(*v),
-                ..default()
-            },));
+                },
+                Generated,
+            ));
         }
 
-        let triangles: Vec<u32> = vec![0, 8, 15, 0, 15, 7, 0, 9, 8, 0, 1, 9];
+        let mut triangles: Vec<u32> = vec![];
+        for i in 0..(config.detail - 1) {
+            #[rustfmt::skip]
+            let base_tris: Vec<u32> = vec![
+            0,8,15,
+            0,15,7,
+            0,9,8,
+            0,1,9,
+            1,10,9,
+            1,2,10,
+            2,11,10,
+            2,3,11,
+            3,12,11,
+            3,4,12,
+            4,13,12,
+            4,5,13,
+            5,14,13,
+            5,6,14,
+            6,15,14,
+            6,7,15,
+        ];
+            for j in base_tris {
+                triangles.push(j + (i * 8) as u32);
+            }
+        }
 
         let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
         mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
@@ -275,14 +237,17 @@ fn build_mesh(
         mesh.compute_flat_normals();
         let handle = meshes.add(mesh);
 
-        commands.spawn(PbrBundle {
-            mesh: handle,
-            material: materials.add(StandardMaterial {
-                base_color: Color::ORANGE_RED,
+        commands.spawn((
+            Generated,
+            PbrBundle {
+                mesh: handle,
+                material: materials.add(StandardMaterial {
+                    base_color: Color::ORANGE_RED,
+                    ..default()
+                }),
                 ..default()
-            }),
-            ..default()
-        });
+            },
+        ));
     }
 }
 
@@ -301,8 +266,8 @@ fn debug(
         .sorted_by_key(|(p, _)| p.0)
         .tuples::<(_, _, _, _)>()
     {
-        let points = (0..=config.detail)
-            .map(|i| i as f32 / config.detail as f32)
+        let points = (0..config.detail)
+            .map(|i| i as f32 / config.detail as f32 - 1.0)
             .map(|t| {
                 cubic_bezier(
                     tform1.translation,
@@ -318,74 +283,74 @@ fn debug(
         draw_polyline(points, &mut painter);
     }
 
-    // if let Some((tfm1, tfm2, tfm3, tfm4)) = point_q
-    //     .iter()
-    //     .sorted_by_key(|(p, _)| p.0)
-    //     .map(|(_, tfm)| tfm)
-    //     .tuples::<(_, _, _, _)>()
-    //     .last()
-    // {
-    //     let vertices = (0..=config.detail)
-    //         .map(|i| i as f32 / config.detail as f32)
-    //         .map(|t| {
-    //             (
-    //                 t,
-    //                 cubic_bezier(
-    //                     tfm1.translation,
-    //                     tfm2.translation,
-    //                     tfm3.translation,
-    //                     tfm4.translation,
-    //                     t,
-    //                 ),
-    //             )
-    //         })
-    //         .flat_map(|(t, curve_point)| {
-    //             // Vertices of one slice of road, relative to the point on the curve
-    //             let local_vertices = vec![
-    //                 Vec3::new(-0.5, 0.3, 0.0),
-    //                 Vec3::new(-0.3, 0.3, 0.0),
-    //                 Vec3::new(-0.2, 0.2, 0.0),
-    //                 Vec3::new(0.2, 0.2, 0.0),
-    //                 Vec3::new(0.3, 0.3, 0.0),
-    //                 Vec3::new(0.5, 0.3, 0.0),
-    //                 Vec3::new(0.5, 0.0, 0.0),
-    //                 Vec3::new(-0.5, 0.0, 0.0),
-    //             ];
-    //             // Map these local points to world points by adding them to the curve point
-    //             local_vertices.into_iter().map(move |local_vertex| {
-    //                 let prev = cubic_bezier(
-    //                     tfm1.translation,
-    //                     tfm2.translation,
-    //                     tfm3.translation,
-    //                     tfm4.translation,
-    //                     t - 0.01,
-    //                 );
-    //                 let next = cubic_bezier(
-    //                     tfm1.translation,
-    //                     tfm2.translation,
-    //                     tfm3.translation,
-    //                     tfm4.translation,
-    //                     t + 0.01,
-    //                 );
-    //                 let local_z = (prev - next).normalize();
-    //                 let local_y = Vec3::Y;
-    //                 let local_x = local_y.cross(local_z).normalize();
-    //                 let mut world_vertex = curve_point;
-    //                 world_vertex += local_vertex.x * local_x;
-    //                 world_vertex += local_vertex.y * local_y;
-    //                 world_vertex += local_vertex.z * local_z;
-    //                 world_vertex
-    //             })
-    //         })
-    //         .collect::<Vec<_>>();
-    //
-    //     painter.hollow = false;
-    //     painter.color = Color::ORANGE;
-    //     for v in vertices {
-    //         painter.set_translation(v);
-    //         painter.circle(0.015);
-    //     }
-    // }
+    if let Some((tfm1, tfm2, tfm3, tfm4)) = point_q
+        .iter()
+        .sorted_by_key(|(p, _)| p.0)
+        .map(|(_, tfm)| tfm)
+        .tuples::<(_, _, _, _)>()
+        .last()
+    {
+        let vertices = (0..config.detail)
+            .map(|i| i as f32 / config.detail as f32 - 1.0)
+            .map(|t| {
+                (
+                    t,
+                    cubic_bezier(
+                        tfm1.translation,
+                        tfm2.translation,
+                        tfm3.translation,
+                        tfm4.translation,
+                        t,
+                    ),
+                )
+            })
+            .flat_map(|(t, curve_point)| {
+                // Vertices of one slice of road, relative to the point on the curve
+                let local_vertices = vec![
+                    Vec3::new(-0.5, 0.3, 0.0),
+                    Vec3::new(-0.3, 0.3, 0.0),
+                    Vec3::new(-0.2, 0.2, 0.0),
+                    Vec3::new(0.2, 0.2, 0.0),
+                    Vec3::new(0.3, 0.3, 0.0),
+                    Vec3::new(0.5, 0.3, 0.0),
+                    Vec3::new(0.5, 0.0, 0.0),
+                    Vec3::new(-0.5, 0.0, 0.0),
+                ];
+                // Map these local points to world points by adding them to the curve point
+                local_vertices.into_iter().map(move |local_vertex| {
+                    let prev = cubic_bezier(
+                        tfm1.translation,
+                        tfm2.translation,
+                        tfm3.translation,
+                        tfm4.translation,
+                        t - 0.01,
+                    );
+                    let next = cubic_bezier(
+                        tfm1.translation,
+                        tfm2.translation,
+                        tfm3.translation,
+                        tfm4.translation,
+                        t + 0.01,
+                    );
+                    let local_z = (prev - next).normalize();
+                    let local_y = Vec3::Y;
+                    let local_x = local_y.cross(local_z).normalize();
+                    let mut world_vertex = curve_point;
+                    world_vertex += local_vertex.x * local_x;
+                    world_vertex += local_vertex.y * local_y;
+                    world_vertex += local_vertex.z * local_z;
+                    world_vertex
+                })
+            })
+            .collect::<Vec<_>>();
+
+        painter.hollow = false;
+        painter.color = Color::ORANGE;
+        for v in vertices {
+            painter.set_translation(v);
+            painter.circle(0.015);
+        }
+    }
 }
 
 fn cubic_bezier(a: Vec3, b: Vec3, c: Vec3, d: Vec3, t: f32) -> Vec3 {
