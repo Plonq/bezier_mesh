a0e1d54c16f649139ba0e0d2bb806c92c0880221
Huon Imberger <huon@plonq.org>
2023-05-04 18:30:19 +1000

    Optimize (don't remove and re-create entity every time)

--- src/main.rs
+++ src/main.rs
@@ -33,13 +33,7 @@ fn main() {
         .add_plugins(DefaultPickingPlugins)
         .add_plugin(TransformGizmoPlugin::default())
         .add_startup_system(setup)
-        .add_systems(
-            (
-                clear.run_if(|config: Res<Config>| config.auto_update),
-                build_mesh.run_if(|config: Res<Config>| config.auto_update),
-            )
-                .chain(),
-        )
+        .add_systems((build_mesh.run_if(|config: Res<Config>| config.auto_update),).chain())
         .run()
 }
 
@@ -59,6 +53,7 @@ struct Config {
     auto_update: bool,
     detail: usize,
     control_points: Vec<Vec3>,
+    mesh: Option<Handle<Mesh>>,
 }
 
 fn setup(
@@ -110,20 +105,13 @@ fn setup(
     }
 }
 
-fn clear(mut commands: Commands, generated_q: Query<Entity, With<Generated>>) {
-    for gen in generated_q.iter() {
-        commands.entity(gen).despawn();
-    }
-}
-
 fn build_mesh(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     point_q: Query<(&ControlPoint, &Transform)>,
-    config: Res<Config>,
+    mut config: ResMut<Config>,
 ) {
-    // println!("Building mesh...");
     if let Some(((_, tfm1), (_, tfm2), (_, tfm3), (_, tfm4))) = point_q
         .iter()
         .sorted_by_key(|(cp, _)| cp.0)
@@ -188,66 +176,76 @@ fn build_mesh(
             .collect::<Vec<_>>();
 
         // debug
-        for v in vertices.iter() {
-            // println!("spawning at: {v}");
-            commands.spawn((
-                PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::UVSphere {
-                        radius: 0.02,
-                        ..default()
-                    })),
-                    material: materials.add(Color::RED.into()),
-                    transform: Transform::from_translation(*v),
-                    ..default()
-                },
-                Generated,
-            ));
-        }
+        // for v in vertices.iter() {
+        //     // println!("spawning at: {v}");
+        //     commands.spawn((
+        //         PbrBundle {
+        //             mesh: meshes.add(Mesh::from(shape::UVSphere {
+        //                 radius: 0.02,
+        //                 ..default()
+        //             })),
+        //             material: materials.add(Color::RED.into()),
+        //             transform: Transform::from_translation(*v),
+        //             ..default()
+        //         },
+        //         Generated,
+        //     ));
+        // }
 
         let mut triangles: Vec<u32> = vec![];
         for i in 0..(config.detail - 1) {
             #[rustfmt::skip]
-            let base_tris: Vec<u32> = vec![
-            0,8,15,
-            0,15,7,
-            0,9,8,
-            0,1,9,
-            1,10,9,
-            1,2,10,
-            2,11,10,
-            2,3,11,
-            3,12,11,
-            3,4,12,
-            4,13,12,
-            4,5,13,
-            5,14,13,
-            5,6,14,
-            6,15,14,
-            6,7,15,
-        ];
+                let base_tris: Vec<u32> = vec![
+                0,8,15,
+                0,15,7,
+                0,9,8,
+                0,1,9,
+                1,10,9,
+                1,2,10,
+                2,11,10,
+                2,3,11,
+                3,12,11,
+                3,4,12,
+                4,13,12,
+                4,5,13,
+                5,14,13,
+                5,6,14,
+                6,15,14,
+                6,7,15,
+            ];
             for j in base_tris {
                 triangles.push(j + (i * 8) as u32);
             }
         }
 
-        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
-        mesh.set_indices(Some(Indices::U32(triangles)));
-        mesh.duplicate_vertices();
-        mesh.compute_flat_normals();
-        let handle = meshes.add(mesh);
+        if let Some(mesh_handle) = &config.mesh {
+            let mesh = meshes.get_mut(mesh_handle).unwrap();
+            mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
+            mesh.set_indices(Some(Indices::U32(triangles)));
+            mesh.duplicate_vertices();
+            mesh.compute_flat_normals();
+        } else {
+            let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+            mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
+            mesh.set_indices(Some(Indices::U32(triangles)));
+            mesh.duplicate_vertices();
+            mesh.compute_flat_normals();
+            let handle = meshes.add(mesh);
 
-        commands.spawn((
-            Generated,
-            PbrBundle {
-                mesh: handle,
-                material: materials.add(StandardMaterial {
-                    base_color: Color::ORANGE_RED,
+            commands.spawn((
+                Generated,
+                PbrBundle {
+                    mesh: handle.clone(),
+                    material: materials.add(StandardMaterial {
+                        base_color: Color::ORANGE_RED,
+                        ..default()
+                    }),
                     ..default()
-                }),
-                ..default()
-            },
-        ));
+                },
+            ));
+
+            config.mesh = Some(handle);
+        }
     }
 }
 
